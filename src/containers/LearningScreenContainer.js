89b6854df7b02fc741bb30d8afc2ea4a48310ce5
Jerome Emilien <60210360+Fodilahy-mena@users.noreply.github.com>
2021-05-17 16:41:47 +0300

    adds switch native language functinality and changes on displayed text (#1)
    * adds switch native language functinality and changes on displayed text
    
    * makes changes on switching native language - uses function from action instead of hooks
    
    * creates a translations folder with an object for any app strings that need to be translated
    
    * doesn't use storybook for now to make asynchronous functions work sooner or later

--- src/containers/LearningScreenContainer.js
+++ src/containers/LearningScreenContainer.js
@@ -1,13 +1,21 @@
 import {connect} from 'react-redux';
 import Learning from '../screens/Learning';
-import {categoryPhrasesRoot, currentCategoryName} from '../redux/selectors';
+import {setLanguageName} from '../redux/actions';
+import {
+  categoryPhrasesRoot,
+  currentCategoryName,
+  nativeLanguageRoot,
+} from '../redux/selectors';
 
 function mapStateToProps(state) {
   return {
     categoryPhrases: categoryPhrasesRoot(state),
     currentCategoryName: currentCategoryName(state),
+    nativeLanguage: nativeLanguageRoot(state),
   };
 }
-const mapDispatchToProps = {};
+const mapDispatchToProps = {
+  setLanguageName,
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Learning);
