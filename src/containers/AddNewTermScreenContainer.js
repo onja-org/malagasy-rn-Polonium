219b83db55811c22b30190a5ef682531f9de15f0
student <jerome.emi@onja.org>
2021-05-18 15:43:06 +0300

    added addNewTerm screen and container with textarea components

--- src/containers/AddNewTermScreenContainer.js
+++ src/containers/AddNewTermScreenContainer.js
@@ -1,5 +1,6 @@
 import {connect} from 'react-redux';
 import AddNewTerm from '../screens/AddNewTerm';
+import {setLanguageName} from '../redux/actions';
 import {categoriesRoot, nativeLanguageRoot} from '../redux/selectors';
 
 function mapStateToProps(state) {
@@ -8,6 +9,8 @@ function mapStateToProps(state) {
     nativeLanguage: nativeLanguageRoot(state),
   };
 }
-const mapDispatchToProps = {};
+const mapDispatchToProps = {
+  setLanguageName,
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(AddNewTerm);
--- src/screens/AddNewTerm.js
+++ src/screens/AddNewTerm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   Text,
   View,
@@ -6,18 +6,46 @@ import {
   SafeAreaView,
   KeyboardAvoidingView,
 } from 'react-native';
+import DropDownPicker from 'react-native-custom-dropdown';
+import {LANGUAGE_NAMES} from '../data/dataUtils';
+import {
+  LANG_DATA,
+  CATEGORY_HEADING,
+  ENGLISH_PHRASE_HEADING,
+  MALAGASY_PHRASE_HEADING,
+  PLACEHOLDER_NEW_TERM,
+  ADD_BUTTON_TEXT,
+} from '../translations';
+
 import ToolBar from '../components/ToolBar/ToolBar';
 import ToolButton from '../components/ToolButton/ToolButton';
 import LanguageSwitcher from '../components/LanguageSwitcher/LanguageSwitcher';
 import BackIcon from '../components/ToolButton/assets/back.svg';
 import ModeIcon from '../components/ToolButton/assets/mode.svg';
+import AddButton from '../components/NextButton/NextButton';
+
+import SectionHeading from '../components/SectionHeading/SectionHeading';
+import Textarea from '../components/Textarea/Textarea';
 
 export default ({
   //nav provider
   navigation,
   categories,
+  setLanguageName,
   nativeLanguage,
 }) => {
+  const [englishPhrase, setEnglishPhrase] = useState('');
+  const [malagasyPhrase, setMalagasyPhrase] = useState('');
+  const [openCategoriesList, setOpenCategoriesList] = useState(false);
+
+  const categoryHeading = LANG_DATA[CATEGORY_HEADING][nativeLanguage];
+  const englishPhraseHeading =
+    LANG_DATA[ENGLISH_PHRASE_HEADING][nativeLanguage];
+  const malagasyPhraseHeading =
+    LANG_DATA[MALAGASY_PHRASE_HEADING][nativeLanguage];
+  const placeholderNewTerm = LANG_DATA[PLACEHOLDER_NEW_TERM][nativeLanguage];
+  const addButtonText = LANG_DATA[ADD_BUTTON_TEXT][nativeLanguage];
+
   return (
     <SafeAreaView style={{flex: 1}}>
       <KeyboardAvoidingView style={{flex: 1}} behavior="padding">
@@ -33,10 +61,70 @@ export default ({
                 </ToolButton>
               }
             />
+            <ToolBar
+              button={
+                <LanguageSwitcher
+                  firstLanguage={LANGUAGE_NAMES.EN}
+                  LeftText={nativeLanguage === LANGUAGE_NAMES.EN ? 'MG' : 'EN'}
+                  RightText={nativeLanguage === LANGUAGE_NAMES.EN ? 'EN' : 'MG'}
+                  color="#FFFFFF"
+                  iconType=""
+                  iconName="swap-horiz"
+                  onPress={() =>
+                    setLanguageName(
+                      nativeLanguage === LANGUAGE_NAMES.EN
+                        ? LANGUAGE_NAMES.MG
+                        : LANGUAGE_NAMES.EN,
+                    )
+                  }
+                  iconSize={24}
+                />
+              }
+            />
+            <ToolBar
+              button={
+                <ToolButton onPress={() => {}}>
+                  <ModeIcon width={24} height={24} fill="#FFFFFF" />
+                </ToolButton>
+              }
+            />
           </View>
-        </View>
-        <View>
-          <Text>Text</Text>
+          <View style={styles.heading}>
+            <SectionHeading text={categoryHeading} />
+            <View onPress={() => setOpenCategoriesList(true)}>
+              <Text>Select a category</Text>
+            </View>
+          </View>
+          <View style={styles.heading}>
+            <SectionHeading text={englishPhraseHeading} />
+          </View>
+          <View style={{marginBottom: 37}}>
+            <Textarea
+              phrase={englishPhrase}
+              editable={true}
+              onChange={text => setEnglishPhrase(text)}
+              placeholder={placeholderNewTerm}
+            />
+          </View>
+          <View style={styles.heading}>
+            <SectionHeading text={malagasyPhraseHeading} />
+          </View>
+          <View style={{marginBottom: 37}}>
+            <Textarea
+              phrase={malagasyPhrase}
+              editable={true}
+              onChange={text => setMalagasyPhrase(text)}
+              placeholder={placeholderNewTerm}
+            />
+          </View>
+          <View style={{marginTop: 45}}>
+            <AddButton
+              isDisabled={true}
+              textColor="#06B6D4"
+              text={addButtonText}
+              onPress={() => {}}
+            />
+          </View>
         </View>
       </KeyboardAvoidingView>
     </SafeAreaView>
@@ -49,6 +137,8 @@ const styles = StyleSheet.create({
     paddingBottom: 56,
   },
   heading: {
+    flexDirection: 'row',
+    alignItems: 'center',
     paddingBottom: 15,
   },
 });
